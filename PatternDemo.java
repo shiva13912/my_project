0640b858151c59363151554c9884c13a7ca516f0
“vivek” <24523751001@mvsrec.edu.in>
2026-03-23 15:48:05 +0530

    Your commit message

---
+++ PatternDemo.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class PatternDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        // Pattern 1: Right triangle
+        System.out.println("\nRight Triangle Pattern:");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // Pattern 2: Inverted triangle
+        System.out.println("\nInverted Triangle Pattern:");
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // Pattern 3: Number pattern
+        System.out.println("\nNumber Pattern:");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
